292afecdac97da3120e9e74dbb87cb7749b89d84
HimanshuP90 <himanshup895@gmail.com>
2017-12-22 17:30:48 +0530

    Added ES6 concept

---
+++ ECMAScript.js
@@ -0,0 +1,13 @@
+//ECMAScript 6
+
+In 1995, JavaScript was originally created by Brendan Ike at Netscape and then was adopted by Microsoft as JScript.
+With different versions of a language for different browsers, there were soon an urgent need to standardize the language. 
+ECMA, or the European Computer Manufacturer Association, is the governing body that provides the ECMAScript specifications for JavaScript browser implementations
+
+1)Transpiling is the process of taking ES6 code that we've written and converting it into ES5, so it can be read by browsers. 
+
+2) Transpiling Done by Babel.js/ Traceur/closure
+
+3) Babel was created by Sebastian McKenzie an Australian developer who now works at Facebook.
+
+Babel is JavaScript compiler
